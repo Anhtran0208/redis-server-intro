808bb4eeed57f8904d27056a4d9b2d1f77d52179
Anh Tran <anhtrannd2004@gmail.com>
2026-05-24 23:25:34 -0700

    done sorted set

--- internal/core/storage.go
+++ internal/core/storage.go
@@ -6,8 +6,10 @@ import (
 
 var dictStore *data_structure.Dict
 var setStore map[string]*data_structure.SimpleSet
+var zsetStore map[string]*data_structure.SortedSet
 
 func init() {
 	dictStore = data_structure.CreateDict()
 	setStore = make(map[string]*data_structure.SimpleSet)
+	zsetStore = make(map[string]*data_structure.SortedSet)
 }
